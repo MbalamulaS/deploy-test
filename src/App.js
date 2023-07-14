241d13e327867d941d15c0f1d162de1ad546e844
MbalamulaS <nyanditi.mbalamula@gmail.com>
2023-07-14 23:10:28 +0300

    improve index page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my first deployed app on Ubuntu server.
         </p>
         <a
           className="App-link"
